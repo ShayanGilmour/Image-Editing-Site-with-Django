1e4f9b6ecbf062f5110f2f4384c02f8c667f02c5
Shayan <shayan.peyghambari@gmail.com>
2019-02-12 23:17:50 +0330

    Probably the last
    Imposter version!

--- fileuploader/models.py
+++ fileuploader/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.utils import timezone
 
 class Req(models.Model):
     image = models.ImageField()
     share = models.BooleanField(default=False);
+    pubDate = models.DateTimeField('date published', null=True)
--- fileuploader/views.py
+++ fileuploader/views.py
@@ -1,8 +1,8 @@
 from random import randint
-
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.core.files.storage import default_storage
+from django.utils import timezone
 from .models import Req
 from PIL import Image
 import os
@@ -10,6 +10,11 @@ import os
 
 def index(request):
     return render(request, 'fileuploader/index.html');
+
+
+def getReq(request):
+    req = Req.objects.filter(pk=request.session['photoId']); req = req[0]; #Finds the uploaded image
+    return req;
 
 
 def upload(request):
@@ -23,7 +28,7 @@ def upload(request):
 
 
 def blackAndWhite(request):
-    req = Req.objects.filter(pk=request.session['photoId']); req = req[0]; #Finds the uploaded image
+    req = getReq(request);
 
     imageTmp = Image.open(req.image); #The image that should be edited
     imageTmp = imageTmp.convert('L'); #Black and White
@@ -35,12 +40,13 @@ def blackAndWhite(request):
         'req': req, 'request': request,
     })
 
+
 def resize(request):
-    req = Req.objects.filter(pk=request.session['photoId']); req = req[0];
+    req = getReq(request);
 
     imageTmp = Image.open(req.image);
-    height = request.POST.get('height', ''); width =  request.POST.get('width',  '');
-    imageTmp = imageTmp.resize(( int(width), int(height) ));
+    height = request.POST.get('height'); width =  request.POST.get('width');
+    imageTmp = imageTmp.resize((int(width), int(height)));
 
     os.remove(req.image.path);
     imageTmp.save(req.image.path);
@@ -50,41 +56,44 @@ def resize(request):
 
 
 def crop(request):
-    req = Req.objects.filter(pk=request.session['photoId']); req = req[0];
+    req = getReq(request);
 
     imageTmp = Image.open(req.image);
-    left = int (request.POST.get('left', ''));
-    upper = int (request.POST.get('upper',  ''));
-    right = int (request.POST.get('right', ''));
-    lower = int (request.POST.get('lower',  ''));
-    imageTmp = imageTmp.crop (( left, upper, right, lower ));
+    left = int (request.POST.get('left'));
+    upper = int (request.POST.get('upper'));
+    right = int (request.POST.get('right'));
+    lower = int (request.POST.get('lower'));
+    imageTmp = imageTmp.crop ((left, upper, right, lower));
 
     os.remove(req.image.path);
     imageTmp.save(req.image.path);
     return render(request, 'fileuploader/edit.html', {
         'req': req, 'request': request,
     })
+
 
 def rotate(request):
-    req = Req.objects.filter(pk=request.session['photoId']); req = req[0];
+    req = getReq(request);
 
     imageTmp = Image.open(req.image);
-    degree = request.POST.get('degree', '');
-    imageTmp = imageTmp.rotate(( int(degree) ));
+    degree = request.POST.get('degree');
+    imageTmp = imageTmp.rotate((int(degree)));
 
     os.remove(req.image.path);
     imageTmp.save(req.image.path);
     return render(request, 'fileuploader/edit.html', {
         'req': req, 'request': request,
     })
+
 
 def share(request):
     if request.method == "POST":
-        req = Req.objects.filter(pk=request.session['photoId']); req = req[0];
+        req = getReq(request);
         req.share = True;
+        req.pubDate = timezone.now(); #Sets the publishing date and time
         req.save();
 
-    shared = Req.objects.filter(share=True);
+    shared = Req.objects.filter(share=True).order_by('-pubDate'); #Sort
     return render(request, 'fileuploader/shared.html', {
         'shared': shared, 'request': request,
     })
